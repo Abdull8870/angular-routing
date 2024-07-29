4095af759cabd3d658b778b12643ec48a4e5b11e
Abdul Rahuman <abrahuman8870@gmail.com>
2024-07-29 01:02:26 -0400

    inherting values from parent router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { TaskComponent } from "./tasks/task/task.component";
 import { TasksComponent } from "./tasks/tasks.component";
 import { NoTaskComponent } from "./tasks/no-task/no-task.component";
 import { UserTasksComponent } from "./users/user-tasks/user-tasks.component";
+import { NewTaskComponent } from "./tasks/new-task/new-task.component";
 
 
 export const routes:Routes=[
@@ -16,6 +17,16 @@ export const routes:Routes=[
     },
     {
         path:'users/:userId',
-        component:UserTasksComponent
+        component:UserTasksComponent,
+        children:[
+            {
+                path:'tasks',
+                component:TasksComponent
+            },
+            {
+                path:'tasks/new',
+                component:NewTaskComponent
+            }
+        ]
     }
 ]
--- src/app/users/user-tasks/user-tasks.component.ts
+++ src/app/users/user-tasks/user-tasks.component.ts
@@ -1,20 +1,35 @@
-import { Component,input,inject,computed } from '@angular/core';
+import { Component,input,inject,computed, OnInit } from '@angular/core';
 import { UsersService } from '../users.service';
+import { ActivatedRoute, RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-user-tasks',
+  imports:[RouterOutlet,RouterLink],
   standalone: true,
   templateUrl: './user-tasks.component.html',
   styleUrl: './user-tasks.component.css',
 })
-export class UserTasksComponent {
+export class UserTasksComponent implements OnInit{
 
   userId=input.required<string>();
   private userService=inject(UsersService);
+  private activeRoute=inject(ActivatedRoute);
+  userName='';
 
-  userName=computed(()=>
-    this.userService.users.find((u)=>
-    u.id===this.userId()
-    )?.name)
+  // userName=computed(()=>
+  //   this.userService.users.find((u)=>
+  //   u.id===this.userId()
+  //   )?.name)
+
+  ngOnInit(): void {
+    this.activeRoute.paramMap.subscribe({
+      next:(param)=>{
+        this.userName=this.userService.users.find((u)=>
+        u.id===param.get('userId')
+      )?.name || ''; 
+    }
+    });
+  
+    }
   
 }
